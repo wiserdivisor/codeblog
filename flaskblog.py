7f00933fc9eeb5d7d6a17eee818c842195d43f39
wiserdivisor <dat0user@gmail.com>
2020-07-29 02:04:29 +0530

    commit_3

--- flaskblog.py
+++ flaskblog.py
@@ -4,7 +4,8 @@
 # log on "localhost:5000"
 
 # importing flask class.
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 # make app variable.
 # set it to an instance of flask class.
@@ -15,6 +16,7 @@ from flask import Flask, render_template, url_for
 # templates and static files.
 # this is instanciated flask application.
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '81e84b26ff8f1015428db0cc672fc862'
 
 posts = [
     {
@@ -40,12 +42,25 @@ posts = [
 @app.route("/") #homepage
 @app.route("/home")
 # function for this route
-def hello():
+def home():
     return render_template("home.html", posts=posts, title="Home")
 
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
